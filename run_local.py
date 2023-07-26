9583fc06441949dd719b4c54406449ee78c4fb2c
MooArnon <oomarnon.000@gmail.com>
2023-07-26 16:15:20 +0700

    feat: db

--- run_local.py
+++ run_local.py
@@ -1,7 +1,19 @@
+import schedule
+
 from web_scaping import ScrapePrice
+from db import DatabaseInsertion
 
-obj = ScrapePrice(r"web_scaping/yahoo_btc_config.json")
+def scrap_data():
+    
+    obj = ScrapePrice(r"web_scaping/yahoo_btc_config.json")
 
-price = obj.get_price()
+    price = obj.get_price()
+    
+    db = DatabaseInsertion()
 
-print(type(price))
+    db.insert_data(
+        element=("app, price"),
+        data = ("test_02", price)
+    )
+    
+schedule.every().minute.at(":17").do(scrap_data)
